1cb450e7aadfbc451038118a5b53e9330b9795f4
JuFlow Digital <juflowdigital@gmail.com>
2026-09-02 17:15:11 -0300

    remove dados demonstrativos e conecta modulos ao banco

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'voltura-shell-v2'
+const CACHE = 'voltura-shell-v3'
 const SHELL = ['./', './Voltua%20ERP.dc.html', './manifest.webmanifest', './favicon.svg']
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())))
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())))
